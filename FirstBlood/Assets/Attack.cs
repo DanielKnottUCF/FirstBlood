64bdd1dca8840d904f64c40b689b5bb3b0521822
Daniel <danielgknott@gmail.com>
2020-07-28 11:39:18 -0400

    Adding Files

---
+++ FirstBlood/Assets/Attack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Attack : NetworkBehaviour
+{
+    [SerializeField]
+    private GameObject AttackProjectilePrefab;
+
+    [SerializeField]
+    private float bulletSpeed;
+
+    void Awake(){
+        //GetComponent<Rigidbody2D>().AddForce(Vector3.forward * bulletSpeed);
+        //Destroy(gameObject, 1.0f);
+    }
+
+    void Update(){
+        if (this.isLocalPlayer && Input.GetKeyDown(KeyCode.Space)){
+            this.CmdShoot();
+        }
+    }
+
+    [Command]
+    void CmdShoot(){
+        Vector3 moveDirection;
+        moveDirection = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+        moveDirection.z = 0;
+        moveDirection.Normalize();
+        //transform.position = (transform.position * bulletSpeed * Time.deltaTime);
+        GameObject AttackProjectile = Instantiate(AttackProjectilePrefab, transform.position + transform.forward*2, transform.rotation);
+        AttackProjectile.GetComponent<Rigidbody2D>().velocity = (moveDirection * bulletSpeed);
+        NetworkServer.Spawn(AttackProjectile);
+        Destroy(AttackProjectile, 2.0f);
+    }
+}
---
+++ FirstBlood/Assets/MovePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MovePlayer : NetworkBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Rigidbody2D rb;
+
+    void FixedUpdate(){
+        if (this.isLocalPlayer){
+            rb = GetComponent<Rigidbody2D>();
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+            Vector2 movement = new Vector2 (x, y);
+            rb.velocity = (movement * speed);
+
+            Vector3 mouseScreen = Input.mousePosition;
+            Vector3 mouse = Camera.main.ScreenToWorldPoint(mouseScreen);
+            transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x) * Mathf.Rad2Deg - 90);
+        }
+    }
+}
